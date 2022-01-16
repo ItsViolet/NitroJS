2049692a9a22523ad6659a0e4ccc7c1f8354d597
XFaon <xfaonae@protonmail.com>
2022-01-16 13:39:22 -0500

    Chore: Removed useless example

--- examples/nitrojs-config-tools/src/main.ts
+++
@@ -1,8 +0,0 @@
-import terminal from "@skylixgh/nitrojs-terminal";
-
-terminal.log("Welcome to your NitroJS starter app");
-terminal.log("About This APP:");
-terminal.log("  Type: Node");
-terminal.log("  Module: CommonJS");
-terminal.log("  Using TypeScript: True");
-terminal.log("Get started by reading the documentation! https://docs.skylix.net/docs");
