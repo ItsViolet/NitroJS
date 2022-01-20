e5bbc479cf78b11c12a86f25036e8769cd3614cc
XFaonAE <xfaonae@protonmail.com>
2022-01-20 06:50:33 -0500

    Chore: Added better rendering utilities

--- packages/nitrojs-terminal/examples/prompts/Index.ts
+++ packages/nitrojs-terminal/examples/prompts/Index.ts
@@ -1,24 +1,44 @@
-import Terminal, { TerminalPrompt, TerminalPromptType } from "../../src/Terminal";
+// import Terminal, { TerminalPrompt, TerminalPromptType } from "../../src/Terminal";
 
-TerminalPrompt.prompt(
-	TerminalPromptType.boolean,
-	"Do you like NitroJS Full Stack Utility Framework?",
-    (answer) => {
-		TerminalPrompt.prompt(
-			TerminalPromptType.string,
-			"What is your name",
-			(answer2) => {
-				Terminal.log("Nice to meet you " + answer2);
-				if (answer) {
-					Terminal.log("We're happy to know you like NitroJS!");
-				} else {
-					Terminal.log(
-						"Awe, well let us know if there is anything we can do to improve NitroJS :)"
-					);
-				}
-			},
-			"XFaon"
-		);
-	},
-	true
-);
+import { TerminalPrompt } from "../../src/Terminal";
+
+// TerminalPrompt.prompt(
+// 	TerminalPromptType.boolean,
+// 	"Do you like NitroJS Full Stack Utility Framework?",
+//     (answer) => {
+// 		TerminalPrompt.prompt(
+// 			TerminalPromptType.string,
+// 			"What is your name",
+// 			(answer2) => {
+// 				Terminal.log("Nice to meet you " + answer2);
+// 				if (answer) {
+// 					Terminal.log("We're happy to know you like NitroJS!");
+// 				} else {
+// 					Terminal.log(
+// 						"Awe, well let us know if there is anything we can do to improve NitroJS :)"
+// 					);
+// 				}
+// 			},
+// 			"XFaon"
+// 		);
+// 	},
+// 	true
+// );
+
+const frames = [ "[     ]", "[ .   ]", "[ ..  ]", "[ ... ]", "[  .. ]", "[   . ]" ];
+let frame = 0;
+
+let rendered = TerminalPrompt.renderLines([" - Hello World", " - Hello NitroJS"]);
+
+setInterval(() => {
+	TerminalPrompt.clearLinesFrom(-rendered);
+
+	frame++;
+
+	if (frame == frames.length) {
+		frame = 0;
+	}
+
+	console.log(` ${frames[frame]} First spinner`);
+	console.log(` ${frames[frame]} Second spinner`);
+}, 100);
--- packages/nitrojs-terminal/src/prompt/TerminalPrompt.ts
+++ packages/nitrojs-terminal/src/prompt/TerminalPrompt.ts
@@ -51,4 +51,36 @@ export default class TerminalPrompt {
     public static get isRunning() {
         return this._isRunning;
     }
+
+    /**
+     * Clear all lines from and below the relative number
+     * @param lineNumberRelative The line number relative to the current cursor position
+     */
+    public static clearLinesFrom(lineNumberRelative: number) {
+        process.stdout.moveCursor(0, lineNumberRelative);
+        process.stdout.clearScreenDown();
+    }
+
+    /**
+     * Render a group or a single line
+     * @param lines The line or lines as an array
+     * @returns Number of lines rendered
+     */
+    public static renderLines(lines: string | string[]) {
+        const linesArray = [];
+        let linesRendered = 0;
+
+        if (Array.isArray(lines)) {
+            linesArray.push(...lines);
+        } else {
+            linesArray.push(lines);
+        }
+
+        linesArray.forEach(line => {
+            process.stdout.write(line + "\n");
+            linesRendered++;
+        });
+
+        return linesRendered;
+    }
 }
