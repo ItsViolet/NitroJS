b21a8ab10303f0b7013f5d83100fa4686f2ee61e
XFaonAE <xfaonae@protonmail.com>
2022-01-23 00:07:35 -0500

    Chore: Added better CLI loader

---
+++ packages/nitrojs-cli/bin/loaders/NitroJS.cjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const { spawn } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+if (!fs.existsSync(path.join(__dirname, "../Binary.js"))) {
+	console.error(
+		"Uh oh... Seems like NitroJS's CLI service wasn't compiled :( Please bug the developers about this mistake"
+	);
+	console.error("Why did this happen?");
+	console.error(
+		"  Mistakes always happen :D the person who published this build may have forgotten to run the build script when publishing"
+	);
+
+	process.exit(0);
+}
+
+spawn(
+    "node",
+    ["--experimental-specifier-resolution=node", path.join(__dirname, "../Binary.js")],
+    {
+        cwd: process.cwd(),
+        stdio: [process.stdin, process.stdout, process.stderr],
+    }
+).on("exit", (code) => process.exit(code));
