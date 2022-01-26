49713e69a482d27ca9c1b3d4271205d6b1177197
XFaonAE <xfaonae@protonmail.com>
2022-01-26 17:19:39 -0500

    Chore: CacheStore will now automaticly compress files

--- packages/nitrojs-cli/bin/utils/cacheStore/CacheStore.ts
+++ packages/nitrojs-cli/bin/utils/cacheStore/CacheStore.ts
@@ -1,5 +1,6 @@
 import fs from "fs-extra";
 import path from "path";
+import { minify } from "terser";
 import { Binary } from "../../Binary";
 
 /**
@@ -40,13 +41,23 @@ export default class CacheStore {
 	 * Write a cache record
 	 * @param pathRelativeToCacheRoot Path that is relative to the .nitrojs folder
 	 * @param dataContents The contents of the cache file
+	 * @param useTerser Whether to use Terser to minify files
 	 * @returns The directory or file path of the new record
 	 */
-	public static writeStore(pathRelativeToCacheRoot: string, dataContents: string): string {
+	public static writeStore(pathRelativeToCacheRoot: string, dataContents: string, useTerser = true): string {
 		pathRelativeToCacheRoot = path.join(this._location, pathRelativeToCacheRoot);
 
 		const afterDirReady = () => {
-			fs.writeFileSync(pathRelativeToCacheRoot, dataContents, {});
+			if (useTerser) {
+				minify(dataContents).then((minified) => {
+					fs.writeFileSync(pathRelativeToCacheRoot, minified.code!);
+				}).catch(() => {
+					fs.writeFileSync(pathRelativeToCacheRoot, dataContents);
+				});
+				return;
+			}
+
+			fs.writeFileSync(pathRelativeToCacheRoot, dataContents);
 		};
 
 		fs.mkdir(path.dirname(pathRelativeToCacheRoot), {
