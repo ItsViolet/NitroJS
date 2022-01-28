22c0959b59567db1a002374d158459f39480fa06
XFaonAE <xfaonae@protonmail.com>
2022-01-27 21:44:26 -0500

    Chore: Ascii logo

--- packages/nitrojs-cli/bin/SkylixLogo.ts
+++ packages/nitrojs-cli/bin/SkylixLogo.ts
@@ -1,24 +1,24 @@
 const skylixLogo = `
-                                                                 pppppppppppc
-                                                              pbbbbbbbbbbbbP
-                                                            pbbbbbbbbbbbbP
-                                                          pbbbbbbbbbbbP
-               pppppppppppp                            pDbbbbbbbbbbbP)
-                bbbbbbbbbbbbp                        pbbbbbbbbbbbbP
-                 )Qbbbbbbbbbbbp                    pbbbbbbbbbbbP
-                   )bbbbbbbbbbbbp               )QbbbbbbbbbbbP
-                     )bbbbbbbbbbbbp           pbbbbbbbbbbbbC
-                       )bbbbbbbbbbbbp       pbbbbbbbbbbbP
-                        pbbbbbbbbbbbP      Sbbbbbbbbbbbb
-                     pDbbbbbbbbbbbP          Sbbbbbbbbbbbb
-                   pbbbbbbbbbbbPC              bbbbbbbbbbbbb
-                 pbbbbbbbbbbbP                   bbbbbbbbbbbbb
-              pbbbbbbbbbbbbP                       bbbbbbbbbbbbp
-            pbbbbbbbbbbbbC                           bbbbbbbbbbbb
-         )QbbbbbbbbbbbP
-       pbbbbbbbbbbbbP
-     pbbbbbbbbbbbbC
-   )bbbbbbbbbbbPC
+                                                                 ···········
+                                                              ··············
+                                                            ·············
+                                                          ·············
+               ············                            ··············
+                ·············                        ·············
+                  ·············                    ·············
+                    ·············               ··············
+                      ············            ·············
+                       ·············        ·············
+                        ·············      ·············
+                     ·············           ·············
+                   ·············               ·············
+                 ·············                   ·············
+              ·············                        ·············
+            ·············                            ············
+         ··············
+       ··············
+     ·············
+   ·············
 `;
 
 export default skylixLogo;
